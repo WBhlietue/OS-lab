ca5b317cb4de4a2b891354b528216b85f51b1ebc
Hakuran <WBhlietue@gmail.com>
2023-09-21 18:04:44 +0800

    history show

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,16 +10,51 @@
 struct CommandHistory
 {
     char args[MAX_LINE + 1][MAX_LINE];
-    char firstLetter;
 };
 
 CommandHistory history[10];
+
+void ToHistory(char **args)
+{
+    static int typedHistory = 0;
+    if (typedHistory == 9)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            history[i] = history[i + 1];
+        }
+        for (int i = 0; i <= MAX_LINE; i++)
+        {
+            if (args[i] == NULL)
+            {
+                break;
+            }
+            strcpy(history[typedHistory].args[i], args[i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i <= MAX_LINE; i++)
+        {
+
+            if (args[i] == NULL)
+            {
+                break;
+            }
+            else
+            {
+                strcpy(history[typedHistory].args[i], args[i]);
+            }
+        }
+        typedHistory++;
+    }
+}
 char *GetHistory(int num)
 {
     char *str = (char *)malloc(sizeof(char) * MAX_LINE);
     for (int i = 0; i <= MAX_LINE; i++)
     {
-        if (strlen( history[num].args[i]) == 0)
+        if (strlen(history[num].args[i]) == 0)
         {
             break;
         }
@@ -37,11 +72,46 @@ void PrintHistory()
         printf("%d. %s\n", (i + 1), GetHistory(i));
     }
 }
+void HistoryInput()
+{
+    char c;
+    do
+    {
+        printf("DON'T PRESS CTRL+D !!!!!!!!!!\n");
+        printf("Input X to call command\n");
+        printf("Input R to call last command\n");
+        printf("Input O to exit history\n");
+        printf("Input P to exit program\n");
+        c = getchar();
+        // scanf("%c", &c);
+        switch (c)
+        {
+        case 'x':
+            break;
+        case 'r':
+        for(int i = 9; i >= 0; i--){
+            if(strlen( GetHistory(i)) > 0){
+                // ToHistory(history[i].args);
+            }
+        }
+            break;
+        case 'o':
+            return;
+        case 'p':
+            exit(0);
+            break;
+
+        default:
+            break;
+        }
+    } while (c != 'O');
+}
 void handle_SIGINT(int sig)
 {
-    write(STDOUT_FILENO, "\nCaught SIGINT, exiting now\n", sizeof("\nCaught SIGINT, exiting now\n"));
+    printf("\nCaught SIGINT, exiting now\n");
     PrintHistory();
-    exit(0);
+    HistoryInput();
+    printf("Enter enter to continue\n");
 }
 
 void setup(char inputBuffer[], char *args[], int *background)
@@ -83,7 +153,6 @@ int main()
     signal(SIGINT, &handle_SIGINT);
     int background;
     char *args[MAX_LINE + 1];
-    int typedHistory = 0;
 
     while (1)
     {
@@ -97,41 +166,42 @@ int main()
         pid_t pid = fork();
         if (pid > 0)
         {
-            if (typedHistory == 9)
-            {
-                for (int i = 0; i < 9; i++)
-                {
-                    history[i] = history[i + 1];
-                }
-                for (int i = 0; i <= MAX_LINE; i++)
-                {
-                    if (args[i] == NULL)
-                    {
-                        break;
-                    }
-                    strcpy(history[typedHistory].args[i], args[i]);
-                }
-            }
-            else
-            {
-                for (int i = 0; i <= MAX_LINE; i++)
-                {
+            // if (typedHistory == 9)
+            // {
+            //     for (int i = 0; i < 9; i++)
+            //     {
+            //         history[i] = history[i + 1];
+            //     }
+            //     for (int i = 0; i <= MAX_LINE; i++)
+            //     {
+            //         if (args[i] == NULL)
+            //         {
+            //             break;
+            //         }
+            //         strcpy(history[typedHistory].args[i], args[i]);
+            //     }
+            // }
+            // else
+            // {
+            //     for (int i = 0; i <= MAX_LINE; i++)
+            //     {
 
-                    if (args[i] == NULL)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        strcpy(history[typedHistory].args[i], args[i]);
-                    }
-                }
-            }
+            //         if (args[i] == NULL)
+            //         {
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             strcpy(history[typedHistory].args[i], args[i]);
+            //         }
+            //     }
+            // }
+            ToHistory(args);
             if (background == 0)
             {
                 int status = 0;
                 wait(&status);
-                typedHistory++;
+                // typedHistory++;
             }
             else
             {
